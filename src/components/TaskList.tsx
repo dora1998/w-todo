4bd0c6104eeecec0f95b3fdfc9547e4edae2b557
dora1998 <dorayaki.app@gmail.com>
2019-04-03 02:41:52 +0900

    data属性によるDOM依存を修正

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-import IconButton from './IconButton'
-import TaskEditBox from './TaskEditBox'
-import TaskName from './TaskName'
+import TaskListItem from './TaskListItem'
 
 import { Task } from '../hooks/useTask'
 
@@ -17,77 +15,33 @@ interface TaskListProps {
 export default (props: TaskListProps) => {
   const [editingId, setEditingId] = React.useState<null | string>(null)
 
-  const handleCheckboxChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const taskId = event.currentTarget.attributes.getNamedItem('data-taskid')
-    if (taskId) {
-      props.onToggleDone(taskId.value, event.currentTarget.checked)
-    }
-  }
-  const handleClickRemove = (event: React.FormEvent<HTMLButtonElement>) => {
-    const taskId = event.currentTarget.attributes.getNamedItem('data-taskid')
-    if (taskId) {
-      props.onClickRemove(taskId.value)
-    }
-  }
-  const handleClickEdit = (event: React.FormEvent<HTMLButtonElement>) => {
-    const taskId = event.currentTarget.attributes.getNamedItem('data-taskid')
-    if (taskId) {
-      setEditingId(taskId.value)
-    }
-  }
-  const handleFinishEdit = (taskId: string, newText: string) => {
-    props.onChangeTaskName(taskId, newText)
+  const handleToggleDone = (task: Task, isDone: boolean) =>
+    props.onToggleDone(task.id, isDone)
+  const handleRemove = (task: Task) => props.onClickRemove(task.id)
+  const handleChangeTaskName = (task: Task, newText: string) =>
+    props.onChangeTaskName(task.id, newText)
+  const handleClickTag = (tag: string) => props.onClickTag(tag)
 
-    setEditingId(null)
-  }
-
-  const getNameView = (task: Task) => {
-    if (editingId === task.id) {
-      return (
-        <TaskEditBox
-          taskId={task.id}
-          initialText={task.name}
-          onFinishEdit={handleFinishEdit}
-        />
-      )
+  const handleChangeIsEditing = (task: Task, newState: boolean) => {
+    if (newState) {
+      setEditingId(task.id)
     } else {
-      return <TaskName task={task} onClickTag={props.onClickTag} />
+      setEditingId(null)
     }
   }
 
   const listItems = props.tasks.map(task => {
-    const nameView = getNameView(task)
     return (
-      <TaskItem key={task.id}>
-        <div className="nameDisplay">
-          <input
-            id={`check_${task.id}`}
-            type="checkbox"
-            checked={task.isDone}
-            onChange={handleCheckboxChange}
-            data-taskid={task.id}
-          />
-          {nameView}
-        </div>
-
-        <IconButton
-          elementParams={{
-            className: 'button flat',
-            'data-taskid': task.id,
-            disabled: editingId === task.id
-          }}
-          onClick={handleClickEdit}
-          icon="edit"
-        />
-        <IconButton
-          elementParams={{
-            className: 'button flat red',
-            'data-taskid': task.id
-          }}
-          onClick={handleClickRemove}
-          icon="trash-alt"
-        />
-      </TaskItem>
+      <TaskListItem
+        key={task.id}
+        task={task}
+        isEditing={editingId === task.id}
+        onToggleDone={handleToggleDone}
+        onChangeTaskName={handleChangeTaskName}
+        onChangeIsEditing={handleChangeIsEditing}
+        onClickRemove={handleRemove}
+        onClickTag={handleClickTag}
+      />
     )
   })
   return <TaskList>{listItems}</TaskList>
@@ -96,28 +50,4 @@ export default (props: TaskListProps) => {
 const TaskList = styled.ul`
   margin: 0;
   padding: 0;
-`
-const TaskItem = styled.li`
-  list-style-type: none;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 4px 8px;
-  border-bottom: 1px solid #aaa;
-
-  &:last-child {
-    border-bottom: none;
-  }
-  .nameDisplay {
-    flex-grow: 1;
-    display: flex;
-    align-items: center;
-  }
-
-  .button.flat {
-    margin-right: 8px;
-  }
-  .button.flat:last-child {
-    margin-right: 0px;
-  }
 `
